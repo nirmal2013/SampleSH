fb9b675b2571925e6f5c9fd850a8e9ee25e86835
Nirmal <nirmal2013@gmail.com>
2017-10-01 09:05:04 +0530

    basic new files
    basic new files

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>
+    Shala Hamsa
+  </title>
+
+
+  <link rel = "stylesheet"
+     type = "text/css"
+     href = "css/style.css" />
+
+</head>
+
+<body>
+  <div>
+    <img src="/images/logo128.jpg" alt="HTML5 Icon" style="width:128px;height:128px;">
+  </div>
+  <div>
+    
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'header.php'; ?>
+
+<h1> Are you buying a flat or villa? </hi>
+  <p> or</p>
+<h1> Are you constructing your house? </h1>
+
+
+<br>
+
+<h1> Are you sure about the construction quality? </h1> <br>
+
+<h3> Reach us to get experts help. Learn more </h3>
+
+
+<?php include "footer.php"; ?>
