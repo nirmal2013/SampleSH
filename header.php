3fa7af4fe75f0e2f4efa0cf17e7c725d88dee49d
Nirmal <nirmal2013@gmail.com>
2017-10-01 11:24:48 +0530

    home pages basics
    home page basics

--- header.php
+++ header.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>
-    Shala Hamsa
-  </title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
 
-  <link rel = "stylesheet"
-     type = "text/css"
-     href = "css/style.css" />
+  </head>
+  <body>
 
-</head>
-
-<body>
-  <div>
-    <img src="/images/logo128.jpg" alt="HTML5 Icon" style="width:128px;height:128px;">
-  </div>
-  <div>
-    
+      <div class="container">
+        <div class="header clearfix">
+          <nav>
+            <ul class="nav nav-pills float-right">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <img src="images/logo128.jpg">
+          <h3 class="text-muted">Shala Hamsa</h3>
+        </div>
